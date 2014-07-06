fb150cc399606d0398967f08b0f674be98721172
nipun <nipun@nec-labs.com>
2014-07-05 22:09:07 -0400

    adding new stuff

--- src/multiThreaded/python_multithreaded_server.py
+++ src/multiThreaded/python_multithreaded_server.py
@@ -2,7 +2,7 @@
 # 
 # Filename: python_multithreaded_server.py
 # Author: Nipun Arora
-# Created: Fri Jul  4 13:37:59 2014 (-0400)
+# Created: Fri Jul  4 18:56:32 2014 (-0400)
 # URL: http://www.nipunarora.net 
 # 
 # Description: 
@@ -16,6 +16,7 @@ import sys, thread, time
 def main(config, errorlog):
     sys.stderr = file(errorlog, 'a')
 
+    """ basically single threaded just for the sake of troubleshooting"""
     for settings in parse(config):
         thread.start_new_thread(server, settings)
 
@@ -28,6 +29,34 @@ def parse(configline):
         parts = line.split()
         settings.append((int(parts[0]), int(parts[1]), parts[2], int(parts[3])))
     return settings
+    
+
+def duplicate(dock_socket,client_socket):
+    client_data = client_socket.recv(1024)
+    sys.stderr.write("[OK] Data received:\n %s \n" % client_data)
+    
+    print "Forward data to local port: %s" % (settings[1])
+    local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    local_socket.connect(('', settings[1]))
+    local_socket.sendall(client_data)
+    
+    print "Get response from local socket"
+    client_response = local_socket.recv(1024)
+    local_socket.close()
+    
+    print "Send response to client"
+    client_socket.sendall(client_response)
+    print "Close client socket"
+    client_socket.close()
+
+    print "Forward data to remote server: %s:%s" % (settings[2],settings[3])
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((settings[2], settings[3]))
+    remote_socket.sendall(client_data)       
+    
+    print "Close remote sockets"
+    remote_socket.close()
+
 
 def server(*settings):
     try:
@@ -41,30 +70,9 @@ def server(*settings):
 
         while True:
             client_socket = dock_socket.accept()[0]
-            client_data = client_socket.recv(1024)
-            sys.stderr.write("[OK] Data received:\n %s \n" % client_data)
+            thread.start_new_thread(duplicate,(dock_socket,client_socket))
+            
 
-            print "Forward data to local port: %s" % (settings[1])
-            local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            local_socket.connect(('', settings[1]))
-            local_socket.sendall(client_data)
-
-            print "Get response from local socket"
-            client_response = local_socket.recv(1024)
-            local_socket.close()
-
-            print "Send response to client"
-            client_socket.sendall(client_response)
-            print "Close client socket"
-            client_socket.close()
-
-            print "Forward data to remote server: %s:%s" % (settings[2],settings[3])
-            remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            remote_socket.connect((settings[2], settings[3]))
-            remote_socket.sendall(client_data)       
-
-            print "Close remote sockets"
-            remote_socket.close()
     except:
         print "[ERROR]: ",
         print sys.exc_info()
