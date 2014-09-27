4b6abb21f5b1a436f8b86022c6526352ac7c280d
Nipun Arora <nipun2512@gmail.com>
2014-09-27 19:11:11 -0400

    modlist.sh

---
+++ scripts/modlist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+modprobe ip6table_filter
+modprobe ip6table_mangle
+modprobe ip6_tables
+modprobe ip6t_REJECT
+modprobe iptable_filter
+modprobe iptable_mangle
+modprobe iptable_nat
+modprobe ip_tables
+modprobe ipt_MASQUERADE
+modprobe ipt_REJECT
+modprobe ipv6  
+modprobe nf_conntrack 
+modprobe nf_conntrack_ipv4 
+modprobe nf_conntrack_ipv6
+modprobe nf_defrag_ipv4
+modprobe nf_defrag_ipv6 
+modprobe nf_nat  
+modprobe xt_multiport
