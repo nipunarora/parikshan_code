b6eee97b7ae8d9479e12863947c51d857ecaba2b
root <root@shadow.(none)>
2014-09-27 20:26:09 -0400

    fixing modlist for updates on any comp

--- scripts/modlist.sh
+++ scripts/modlist.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-modprobe vzrst
-modprobe vzcpt
 modprobe ip6table_filter
 modprobe ip6table_mangle
 modprobe ip6_tables
@@ -19,3 +17,8 @@ modprobe nf_defrag_ipv4
 modprobe nf_defrag_ipv6 
 modprobe nf_nat  
 modprobe xt_multiport
+modprobe vzrst
+modprobe vzcpt
+service iptables stop
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o $1 -j MASQUERADE
