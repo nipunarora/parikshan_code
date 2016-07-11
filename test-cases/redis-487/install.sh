bb959b472d8e6c6941bd40ddb8f728126e453501
Nipun Arora <nipun2512@gmail.com>
2016-07-11 00:27:21 +0000

    adding install and run script

---
+++ test-cases/redis-487/install.sh
@@ -0,0 +1,15 @@
+# install.sh --- 
+# 
+# Filename: install.sh
+# Description: 
+# Author: Nipun Arora
+# Maintainer: 
+# Created: Thu Jun 30 01:55:46 2016 (+0000)
+# Code:
+
+git clone https://github.com/antirez/redis.git
+cd redis
+git checkout 2ac546e00cd4000506558e23d11acafb4ce654b7
+cd ..
+ 
+# install.sh ends here
---
+++ test-cases/redis-487/run.sh
@@ -0,0 +1,14 @@
+# run.sh --- 
+# 
+# Filename: run.sh
+# Description: 
+# Author: Nipun Arora
+# Maintainer: 
+# Created: Mon Jul  4 23:27:55 2016 (+0000)
+# Code:
+
+
+
+
+# 
+# run.sh ends here
