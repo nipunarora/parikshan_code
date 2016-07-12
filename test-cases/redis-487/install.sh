3271c5cc62cf8b0fe5c6721768c3fe4589c8d5d7
Nipun Arora <nipun2512@gmail.com>
2016-07-12 00:17:13 +0000

    updated redis install file

--- test-cases/redis-487/install.sh
+++ test-cases/redis-487/install.sh
@@ -7,9 +7,15 @@
 # Created: Thu Jun 30 01:55:46 2016 (+0000)
 # Code:
 
-git clone https://github.com/antirez/redis.git
+if [ ! -d redis ] 
+then
+    echo "Cloning Git Repository of redis"
+    git clone https://github.com/antirez/redis.git
+    cd redis
+    git checkout 2ac546e00cd4000506558e23d11acafb4ce654b7
+    cd ..
+fi
 cd redis
-git checkout 2ac546e00cd4000506558e23d11acafb4ce654b7
-cd ..
+make
  
 # install.sh ends here
